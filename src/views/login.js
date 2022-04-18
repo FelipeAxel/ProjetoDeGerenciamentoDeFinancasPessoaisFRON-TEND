c6b2af15ebbd3293bd1aba4d9ee6b7ba5f49d2b1
FelipeAxel <axelnovais96@gmail.com>
2022-04-18 17:36:57 -0300

    Inicio da implementação das rotas/ form cad-usuario/login

--- src/views/login.js
+++ src/views/login.js
@@ -4,45 +4,63 @@ import FormGroup from "../components/form-group";
 
 
 class Login extends React.Component {
+    state = {
+        email: '',
+        senha: ''
+    }
+
+
+
+    entrar = () => {
+        console.log('Email', this.state.email)
+        console.log('Senha', this.state.senha)
+    }
+
     render() {
         return (
-            <div calssNmae="container">
-                <div className="row">
-                    <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
-                        <div className="bs-docs-section">
-                            <Card title="Login">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="bs-component">
-                                            <fieldset>
-                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                    <input type="email"
-                                                        className="form-control"
-                                                        id="exampleInputEmail1"
-                                                        aria-describedby="emailHelp"
-                                                        placeholder="Digite o Email" />
-                                                </FormGroup>
 
-                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                    <input type="senha"
-                                                        className="form-control"
-                                                        id="exampleInputPassword1"
-                                                        placeholder="Password" />
-                                                </FormGroup>
+            <div className="row">
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                <input type="email"
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({ email: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Digite o Email" />
+                                            </FormGroup>
 
-                                                <button onclick="window.location.href='home.html'" type="button"
-                                                    className="btn btn-success">Entrar</button>
-                                                <button onclick="window.location.href='usuarios.html'" type="button"
-                                                    className="btn btn-danger">Cadastrar</button>
-                                            </fieldset>
-                                        </div>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({ senha: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputPassword1"
+                                                    placeholder="Password" />
+                                            </FormGroup>
+
+                                            <button onClick={this.entrar} className="btn btn-success">
+                                                <i className="pi pi-sign-in"></i>Entrar</button>
+                                            <button onClick={this.prepareCadastrar}
+                                                className="btn btn-danger">
+                                                <i className="pi pi-plus"></i>  Cadastrar
+                                            </button>
+                                        </fieldset>
                                     </div>
                                 </div>
-                            </Card>
-                        </div>
+                            </div>
+                        </Card>
                     </div>
                 </div>
             </div>
+
         )
     }
 }
