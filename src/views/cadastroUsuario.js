eb115e3017f9f18584a56a3004c3112ac45faa3a
FelipeAxel <axelnovais96@gmail.com>
2022-04-20 18:01:07 -0300

    conclusao do caso de uso login, todas validações funcionando

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -2,6 +2,9 @@ import React from "react";
 import {withRouter} from 'react-router-dom';
 import Card from "../components/cards";
 import FormGroup from "../components/form-group";
+import {mensagemErro, mensagemSucesso} from '../components/toastr';
+
+import UsuarioService from "../service/usuarioService";
 
 class CadastroUsuario extends React.Component {
 
@@ -13,9 +16,58 @@ class CadastroUsuario extends React.Component {
         senhaRepeticao: ''
     }
 
-    cadastrar = () => {
-        console.log(this.state)
+    constructor(){
+        super();
+        this.service = new UsuarioService();
     }
+
+    validar(){
+        const msgs=[]
+        if(!this.state.nome){
+            msgs.push('O campo Nome é obrigatorio.')
+        }
+
+        if(!this.state.email){
+            msgs.push('O campo Email é obrigatorio.')
+        }else if(!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            msgs.push('Informe um email valido.')
+        }
+
+
+        if(!this.state.senha|| !this.state.senhaRepeticao){
+            msgs.push('Digite a senha 2x.')
+        }else if(this.state.senha !== this.state.senhaRepeticao){
+            msgs.push('As senhas não batem')
+        }
+        return msgs;
+    }
+
+
+    cadastrar = () => {
+        const msgs = this.validar();
+
+        if(msgs && msgs.length >0){
+            msgs.forEach((msgs, index) =>{
+                mensagemErro(msgs)
+            });
+            return false;
+        }
+
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        this.service.salvar(usuario)
+            .then(response =>{
+                mensagemSucesso('Usuario cadastrado com sucesso, pronto para acessar o sistema')
+                this.props.history.push('/login')
+            }).catch(error=>{
+                mensagemErro(error.response.data)
+            })
+    }
+
+
     cancelar =()=>{
         this.props.history.push('/login')
     }
